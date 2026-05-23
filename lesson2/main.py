dde5abcee9307a2ea5d5ecd3135b5692f01a227b
justinwang19750323-11505test <justinwang19750323@gmail.com>
2026-05-23 19:03:42 +0800

    T

--- lesson2/main.py
+++ lesson2/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from lesson2!")
+from google import genai
 
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(GEMINI_API_KEY)
 
-if __name__ == "__main__":
-    main()
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
+)
+print(response.text)
+
