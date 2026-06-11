fb0d406a08897f1bbde9fb4fb40ac17e8759843a
justinwang19750323-11505test <justinwang19750323@gmail.com>
2026-06-11 23:15:57 +0800

    t

--- lesson2/main.py
+++ lesson2/main.py
@@ -7,5 +7,5 @@ load_dotenv()
 client = genai.Client()
 
 response = client.models.generate_content(
-    model="gemini-2.5-pro", contents="膽結石的原因與治療方式")
+    model="gemini-2.5-pro", contents="介紹世界人口由大至小")
 print(response.text)
